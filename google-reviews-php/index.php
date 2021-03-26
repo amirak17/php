4839172f26ff68d59c8ef1747f8677df747e796c
amirak17 <amir.aakhan@gmail.com>
2021-03-26 12:15:10 -0400

    $1 update

---
+++ google-reviews-php/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$maps_key = 'AIzaSyCSBvH1CjXKNAEHa1msyGpC4nzrlEyiUFE';
+$places_key = 'ChIJCQnWRY6ipBIRWI18NnMnK6k';
+
+$url = 'https://maps.googleapis.com/maps/api/place/details/json?key='.$maps_key.'&placeid='.$places_key;
+$ch = curl_init();
+curl_setopt ($ch, CURLOPT_URL, $url);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec ($ch);
+
+$res        = json_decode($result, true);
+$reviews    = $res['result']['reviews'];
+
+echo '<pre>'; print_r($reviews); echo '</pre>';
+
+?>
+
