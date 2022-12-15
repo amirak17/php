bee9042fe19575c2b3af47bb0bdf5b7a47d66229
Amir Ali Khan <amir.aakhan@gmail.com>
2022-12-15 12:53:47 -0500

    formatBytes

--- useful-functions/string-functions.php
+++ useful-functions/string-functions.php
@@ -399,4 +399,15 @@ function is_dir_empty($dir) {
   return (count(scandir($dir)) == 2);
 }
 
+function formatBytes($size, $precision = 2) {
+  if($size > 0) {
+      $base = log($size, 1024);
+      $suffixes = array('', 'KB', 'MB', 'GB', 'TB');   
+      return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+  }
+  else {
+    return '0 Bytes';
+  }
+}
+
 ?>
